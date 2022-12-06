661b1dd4018c1daf103d510a667d36fc037b669d
Marco Dalla Stella <marco@dallastella.name>
2022-12-06 22:47:11 +0100

    [Day3] Preparation

--- src/advent_of_clerk/day_03.clj
+++ src/advent_of_clerk/day_03.clj
@@ -1,3 +1,16 @@
 ;; # 🎄 Advent of Clerk: Day 3
 (ns advent-of-clerk.day-03
-  (:require [nextjournal.clerk :as clerk]))
+  (:require [nextjournal.clerk :as clerk]
+            [clojure.java.io :as io]
+            [clojure.string :as str]))
+
+(def base-path "./inputs/day3")
+
+(clerk/md (slurp (str base-path "/text.md")))
+
+(def input
+  (->> (io/reader (str base-path "/input.txt"))
+       line-seq
+       (map #(str/split % #"\s"))))
+
+;; # Solution
